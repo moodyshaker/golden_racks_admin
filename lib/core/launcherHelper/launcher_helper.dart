ae3135784e0c31ab7bfcfca4200149685304abf3
ahmedmedhat107201 <ahmedmedhat1072001@gmail.com>
2023-11-07 22:28:55 +0200

    last update

--- lib/core/launcherHelper/launcher_helper.dart
+++ lib/core/launcherHelper/launcher_helper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -8,7 +10,7 @@ class LauncherHelper {
       await launch(url);
     else
       null;
-      // showDefaultErrorMessage();
+    // showDefaultErrorMessage();
   }
 
   static Future<void> launchPhone(String phone) async {
